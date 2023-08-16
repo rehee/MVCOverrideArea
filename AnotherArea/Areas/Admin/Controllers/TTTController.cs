957f3217216ded434b9ecd4c946c1521b33b72a2
xh <x3iii131@gmail.com>
2023-08-16 18:12:11 +0100

    add another controller project to test nested blazor

---
+++ AnotherArea/Areas/Admin/Controllers/TTTController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnotherArea.Areas.Admin.Controllers
+{
+  [Area("Admin")]
+  public class TTTController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
