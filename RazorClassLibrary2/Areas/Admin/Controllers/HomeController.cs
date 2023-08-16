ab5f740b7b9c44909cc7878ddfb6d614331f640b
xh <x3iii131@gmail.com>
2023-08-16 14:39:55 +0100

    init project

---
+++ RazorClassLibrary2/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace RazorClassLibrary1.Areas.Admin.Controllers
+{
+  [Area("Admin")]
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+  }
+}
---
+++ RazorClassLibrary2/Areas/MyFeature/Pages/Page1.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorClassLibrary2.MyFeature.Pages
+{
+  public class Page1Model : PageModel
+  {
+    public void OnGet()
+    {
+
+    }
+  }
+}
